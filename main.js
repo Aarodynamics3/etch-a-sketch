2211295392055ccc456e2cf06b07dae098b483e7
Aaron Hanrahan <hanrahanaaron@gmail.com>
2020-01-23 20:17:31 -0600

    Added clear button!

--- main.js
+++ main.js
@@ -16,3 +16,19 @@ for (let i = 0; i < 308; i++) {
     });
     etch_screen.appendChild(square);
 }
+
+const second_container = document.querySelector('.second-container');
+const button_container = document.createElement('div');
+const button = document.createElement('button');
+button.className = 'reset-button';
+button_container.className = 'button-container';
+button.textContent = 'Clear';
+button.addEventListener('click', function(e) {
+    const squares = document.getElementsByClassName('square-colored');
+    const num = squares.length;
+    for (let j = 0; j < num; j++) {
+        squares.item(0).className = 'square';
+    }
+});
+button_container.appendChild(button);
+second_container.appendChild(button_container);
